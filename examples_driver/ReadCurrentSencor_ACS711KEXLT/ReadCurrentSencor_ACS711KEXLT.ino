ffd560da1ac861d8785d1e830b00127c077460d5
AleksandraFomina <sashavera1998@gmail.com>
2023-07-14 16:19:45 +0300

    modified:   examples_driver/ReadCurrentSencor_ACS711KEXLT/ReadCurrentSencor_ACS711KEXLT.ino

--- examples_driver/ReadCurrentSencor_ACS711KEXLT/ReadCurrentSencor_ACS711KEXLT.ino
+++ examples_driver/ReadCurrentSencor_ACS711KEXLT/ReadCurrentSencor_ACS711KEXLT.ino
@@ -10,21 +10,23 @@ void setup() {
   analogReadResolution(12);
   Serial.begin(115200);
 
-  pinMode(PC1, INPUT);
-  pinMode(PC2, INPUT);
-  pinMode(PC3, INPUT);
+  pinMode(PC1, INPUT); //ph A
+  pinMode(PC2, INPUT); // ph B
+  pinMode(PC3, INPUT); // ph C
   
   digitalWrite(EN_GATE, LOW);
   delay(10);
   digitalWrite(EN_GATE, HIGH);
   delay(10);
   
+  //посчитаем offset для каждой из фаз
   for (int i = 0; i < 64; i++) {
     i_A_offset += (3.3f * (float)analogRead(PC1) / 4096.0f);
     i_B_offset += (3.3f * (float)analogRead(PC2)/ 4096.0f);
     i_C_offset += (3.3f * (float)analogRead(PC3) /4096.0f);
     delay(1);
 }
+//усредняем
 i_A_offset /= 64.0f;
 i_B_offset /= 64.0f;
 i_C_offset /= 64.0f;
@@ -32,6 +34,7 @@ i_C_offset /= 64.0f;
 
 float i_A, i_B, i_C;
 void loop() {
+  //I = U/R
   i_A = (3.3f*(float)analogRead(PC1)/4096.0f - i_A_offset )/shunt_resistor; //A
   i_B = (3.3f*(float)analogRead(PC2)/4096.0f- i_B_offset )/shunt_resistor; //A
   i_C = (3.3f*(float)analogRead(PC3)/4096.0f- i_C_offset )/shunt_resistor; //A
